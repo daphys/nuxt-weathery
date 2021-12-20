a7aa1c044a6502f06b629eb802c2f6aaed688838
Vlad <floppahear@gmail.com>
2021-12-20 10:13:28 +0300

    F1.0

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-let input = document.getElementById('input')
-let button = document.getElementById('button')
-let freespace = document.getElementById('space')
-let themeButto = document.getElementById('wbutton')
-let welcometext = document.getElementById('welcome-text')
+let input = document.getElementById("input");
+let button = document.getElementById("button");
+let freespace = document.getElementById("space");
+let themeButto = document.getElementById("wbutton");
+let welcometext = document.getElementById("welcome-text");
 
-function getCityName(){
-
-let fetchingData = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=599c3ac16baaa0223e348843860f7603`)
-    .then(resp => resp.json())
-    .then(data => freespace.innerHTML = `Right now ${Math.floor(data['main']['temp'])}°C in ${input.value}`);
-
+function getCityName() {
+  let fetchingData = fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=599c3ac16baaa0223e348843860f7603`
+  )
+    .then((resp) => resp.json())
+    .then(
+      (data) =>
+        (freespace.innerHTML = `Right now ${Math.floor(
+          data["main"]["temp"]
+        )}°C in ${input.value}`)
+    );
 }
 
-themeButto.onclick = () =>{
-    document.body.classList.toggle('white')
-}
+themeButto.onclick = () => {
+  document.body.classList.toggle("white");
+};
 
-
-button.addEventListener('click', getCityName)
+button.addEventListener("click", getCityName);
